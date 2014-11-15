9f7c8301da2713dc8136c6f00819e8f82e39ac28
matthieubart <matthieubart14@gmail.com>
2014-11-15 18:11:14 +0100

    Decompte

--- app.js
+++ app.js
@@ -17,9 +17,11 @@ var numeroQuestion=1;
 var idsQuestions = genererIdsQuestions(1,NB_QUESTIONS_BASE);//fonction définie plus bas
 
 var countdown = {};
-for(i=1;i<11;i++){
+for(i=2;i<11;i++){
     countdown[i]=30;
 }
+
+var timer;
 
 //Connexion MongoDB
 MongoClient.connect(url, function(err, db) {
@@ -29,44 +31,11 @@ MongoClient.connect(url, function(err, db) {
     });
 
     io.sockets.on('connection', function (socket, pseudo) {
-        //Si le client se déconnecte, on efface ses données
-        socket.on('disconnect', function () {
-            pseudo = pseudos[socket.id];
-            delete scores[socket.id+";"+pseudo];
-            delete pseudos[socket.id];
-            if(Object.keys(pseudos).length==1){
-                //io.sockets.emit('deconnexion_joueur');
-                io.sockets.emit('scores', scores);
-            }
-        });
-
-        socket.on('nouveau_client', function(pseudo) {
-            pseudo = ent.encode(pseudo);
-
-            pseudos[socket.id] = pseudo;
-
-            scores[socket.id+";"+pseudo] = 0;
-
-            io.sockets.emit('scores', scores);
-
-            if(Object.keys(pseudos).length==2 && numeroQuestion==1){//Object.keys(quesArr).length : taille tableau associatif
-                io.sockets.emit('decompte_debut_partie');
-            }else if(Object.keys(pseudos).length>2){
-                socket.emit('parti_deja_commence');
-                delete scores[socket.id+";"+pseudo];
-                delete pseudos[socket.id];
-                io.sockets.emit('scores', scores);
-            }else{
-                socket.emit('attente');
-            }
-        });
-
-        socket.on('debut_partie', function(){
-
-            //Compte à rebours questions
-            var timer = setInterval(function() {
+        //Compte à rebours questions
+        timer = setInterval(function() {
+            if(timer["_idleTimeout"]==1000 && Object.keys(pseudos).length>0){
                 if(Object.keys(pseudos).length>0){
-                    countdown[numeroQuestion] = countdown[numeroQuestion]-0.5;//2 joueurs donc 0.5 du temps pour chaque joueurs : 1 seconde
+                    countdown[numeroQuestion] = countdown[numeroQuestion]-1;//2 joueurs donc 0.5 du temps pour chaque joueurs : 1 seconde
                 }
                 io.sockets.emit('decompte', { countdown: countdown[numeroQuestion] });
                 if(countdown[numeroQuestion]==0){
@@ -80,19 +49,52 @@ MongoClient.connect(url, function(err, db) {
                     }else{
                         var vainqueur = chercherVainqueur(scores);//fonction chercherVainqueur définie plus bas
                         io.sockets.emit('vainqueur', vainqueur);
-                        //Tout réinitialiser
-                        /*clearInterval(timer);
-                        for(i=1;i<11;i++){
-                            countdown[i]=4;
-                        }
-                        numeroQuestion=1;
-                        idsQuestions = genererIdsQuestions(1,NB_QUESTIONS_BASE);//fonction définie plus bas
-                        while (pseudos.length) { pseudos.pop(); }
-                        while (scores.length) { scores.pop(); }*/
                     }
                 }
-            }, 1000);
+            }else{
+                clearInterval(timer);
+            }
+        }, 1000);
 
+        //Si le client se déconnecte, on efface ses données
+        socket.on('disconnect', function () {
+            pseudo = pseudos[socket.id];
+            delete scores[socket.id+";"+pseudo];
+            delete pseudos[socket.id];
+            if(Object.keys(pseudos).length==1){
+                //io.sockets.emit('deconnexion_joueur');
+                io.sockets.emit('scores', scores);
+            }else if(Object.keys(pseudos).length==0){
+                clearInterval(timer);
+                for(i=2;i<11;i++){
+                    countdown[i]=30;
+                }
+                numeroQuestion=1;
+                idsQuestions = genererIdsQuestions(1,NB_QUESTIONS_BASE);//fonction définie plus bas
+            }
+        });
+
+        socket.on('nouveau_client', function(pseudo) {
+            pseudo = ent.encode(pseudo);
+            pseudos[socket.id] = pseudo;
+            scores[socket.id+";"+pseudo] = 0;
+
+            io.sockets.emit('scores', scores);
+
+            if(Object.keys(pseudos).length==2 && numeroQuestion==1){//Object.keys(quesArr).length : taille tableau associatif
+                countdown[1]=36;
+                io.sockets.emit('decompte_debut_partie');
+            }else if(Object.keys(pseudos).length>2){
+                socket.emit('parti_deja_commence');
+                delete scores[socket.id+";"+pseudo];
+                delete pseudos[socket.id];
+                io.sockets.emit('scores', scores);
+            }else{
+                socket.emit('attente');
+            }
+        });
+
+        socket.on('debut_partie', function(){
             //Cherche la question dans la base de données
             findQuestion(db, idsQuestions[numeroQuestion], function(resultat) {//requete pour la question
                 io.sockets.emit('question', "Question : "+numeroQuestion+"/10 : "+resultat.intitule+" ? "+resultat.reponse+"."+resultat.id_questions);//cacher la réponse une fois fini et l'id
